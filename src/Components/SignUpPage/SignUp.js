f0b981659020306f57873cf46027f39dadb9a6bc
Uttkarsh <uttkarshsaini10@gmail.com>
2023-08-06 23:57:10 +0530

    Working on signup

--- src/Components/SignUpPage/SignUp.js
+++ src/Components/SignUpPage/SignUp.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import fb_logo from '../../Images/fb.png'
 import '../SignUpPage/SignUp.css'
 
 function SignUp() {
   return (
     <div>
-        <div>
-        <img src={fb_logo} alt="" width="15px"/><button className="login__button">Log in with Facebook</button>
-        </div>
-        <div className="or__div">
-            <div className="or__line"></div>
-            <div className="login__or">OR</div> 
-            <div className="or__line"></div>
-        </div>
         <input className="input__text" placeholder="Mobile Number or Email"/>
         <input className="input__text" placeholder="Full Name"/>
         <input className="input__text" placeholder="Username"/>
